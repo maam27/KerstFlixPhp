4e1d2d75f8c6afbffe488bb17e6fc0beecee9683
maam27 <martkools@live.nl>
2017-12-19 14:00:28 +0100

    login voortgang

--- login.php
+++ login.php
@@ -9,27 +9,33 @@ $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;
 ?>
 
 <?php
-//$_POST['username'] = 'a.nunc@seddolor.com';
-//$_post['password'] = 'In';
-
 if(ISSET($_POST['username']) && ISSET($_POST['password'])){
     $userName = $_POST['username'];
     $password = $_POST['password'];
 
+    $password = 'bob';
     echo 'a.nunc@seddolor.com <br>';
     echo 'In<br>';
 
     $data = $dbh->query("SELECT user_name FROM Customer where customer_mail_address = '{$userName}' and password = '{$password}'");
     $result = $data->fetch();
 
+    //debug
+    echo '<br>';
+    var_dump($data);
+    echo '<br>';
+    echo $result['user_name'];
     echo count(count($result['user_name']));
+    echo '<br>';
+    //end debug
+
 
     if(count(count($result['user_name'])) == 1){
         session_start();
         $_SESSION['user']=$result['user_name'];
-        echo 'a';
+        echo '<p>a</p>';
     }else{
-        echo 'b';
+        echo '<p>b</p>';
     }
 }
 ?>
