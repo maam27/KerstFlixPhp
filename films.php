0ef36eeee5d99743e44a277b71bb3abc6a962c70
maam27 <martkools@live.nl>
2018-01-12 16:22:43 +0100

    film genre filter missing image picture

--- films.php
+++ films.php
@@ -16,6 +16,8 @@ require_once 'php/film-functions.php';
 $dbh = db_connect();
 session_start();
 
+//requireLogin
+
 //load navigation menu
 include_once 'partial/nav.php';
 ?>
@@ -23,17 +25,42 @@ include_once 'partial/nav.php';
 <div class="movies">
     <div class="full-width"">
         <h1>Filmoverzicht</h1>
-        <p>filters</p>
+
+        <!--form voor filter opties-->
+        <form name="filter" action="" method="get">
+            <p>filters</p>
+            <select name="genre">
+                <option value="">alle</option>
+                    <?php
+                    foreach(get_all_film_genres($dbh) as $genre){
+                        //autoselect the currently selected in the dropdown
+                        $selected="";
+                        if(isset($_GET['genre'])) {
+                            if($genre == $_GET['genre']){
+                            $selected = 'selected';
+                            }
+                        }
+                        echo '<option '.$selected.'>'.$genre."</option>";
+                    }
+                    ?>
+            </select>
+            <button>filter</button>
+       </form>
     </div>
 
     <!-- filmId, naam, img-->
-    <div class="flex" style="flex-wrap:wrap;justify-content: space-around">
+    <div class="flex" style="flex-wrap:wrap;">
         <?php
-
-            //requireLogin
-
             $filter="";
-            $order = "movie_id desc";
+            //apply filters
+            //filter genre
+            if(isset($_GET['genre'])) {
+                if(!empty($_GET['genre'])){
+                    $filter .= "and movie_genre.genre_name = '" . $_GET['genre']."'";
+                }
+            }
+
+            $order = "movie.movie_id desc";
             echo getFilms($dbh,$filter,$order);
         ?>
     </div>
